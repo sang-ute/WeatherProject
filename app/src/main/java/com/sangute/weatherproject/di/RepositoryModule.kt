3376da7a8240ffa75474e3e9e6700e8235687b0a
Jason Ho <22110020@student.hcmute.edu.vn>
2025-05-28 11:46:09 +0700

    feat: add RepositoryModule for dependency injection of WeatherRepository

---
+++ app/src/main/java/com/sangute/weatherproject/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.sangute.weatherproject.di
+
+import com.sangute.weatherproject.data.repository.WeatherRepositoryImpl
+import com.sangute.weatherproject.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+} 
