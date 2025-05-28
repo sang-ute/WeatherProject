a51a339a3ddea01bb756cd0da4a22741b6fcb4ad
Jason Ho <22110020@student.hcmute.edu.vn>
2025-05-28 11:47:08 +0700

    feat: integrate Hilt for WorkManager configuration in WeatherApplication

--- app/src/main/java/com/sangute/weatherproject/WeatherApplication.kt
+++ app/src/main/java/com/sangute/weatherproject/WeatherApplication.kt
@@ -3,10 +3,22 @@ package com.sangute.weatherproject
 import android.app.Application
 import android.widget.Toast
 import com.google.firebase.FirebaseApp
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class WeatherApplication : Application() {
+class WeatherApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
     override fun onCreate() {
         super.onCreate()
         // Initialize Firebase
